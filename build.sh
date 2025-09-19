d1ac928d17a29cb39599c42d7048cdd3b2f74927
Johannes Berthel <j.berthel@codeblick.de>
2025-09-19 13:52:04 +0200

    feat: crossplatform build

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker build . \
-  -t codeblick/aws-cli:latest \
-  -q
--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-
-docker push codeblick/aws-cli:latest
